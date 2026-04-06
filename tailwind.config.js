b5aa9f175409c5f5a8d2146d0d59ea05ab6c2423
hajipourmohsen2012-sudo <hajipour.mohsen2012@gmail.com>
2026-04-07 02:54:21 +0330

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF5733',
+        secondary: '#33FF57',
+        accent: '#3357FF',
+      },
+    },
+  },
+  plugins: [],
+}
